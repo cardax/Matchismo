6c8ddcdca09bd3cd5d9aba0415ab50932a8ef826
Giuseppe Cardace <cardax@MacBook-Air-di-Giuseppe.local>
2013-04-01 20:24:02 +0200

    Working Assignment 0 + Lecture 3 - Ready for Assignment 1

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -8,52 +8,63 @@
 
 #import "CardGameViewController.h"
 #import "PlayingCardDeck.h"
-
+#import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property (strong, nonatomic) Deck *deck;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property  (strong, nonatomic) CardMatchingGame *game;
 @end
 
 @implementation CardGameViewController
 
-
--(Deck*) deck{
-    if (!_deck) {
-        _deck = [[PlayingCardDeck alloc]init];
+-(CardMatchingGame*)game{
+    if (!_game) {
+        _game = [[CardMatchingGame alloc]initWithCardCount:[self.cardButtons count] usingDeck:[[PlayingCardDeck alloc]init]];
     }
-    return _deck;
+    return _game;
 }
+
 
 
 -(void)setCardButtons:(NSArray *)cardButtons
 {
     _cardButtons=cardButtons;
-    for (UIButton *cardButton in cardButtons){
-        Card *card = [self.deck drawRandomCard];
-        [cardButton setTitle:card.contents
-                    forState:UIControlStateSelected];
+    [self updateUI];
+}
+
+-(void)updateUI{
+    for (UIButton *cardButton in self.cardButtons) {
+        Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
+        cardButton.selected=card.isFaceUp;
+        cardButton.enabled=!card.isUnplayable;
+        cardButton.alpha=card.isUnplayable ? 0.3 : 1;
     }
+    self.scoreLabel.text=[NSString stringWithFormat:@"Score: %d", self.game.score];
 }
 
 - (IBAction)flipCard:(UIButton *)sender {
-    sender.selected = !sender.selected;
-/*
-    sender.selected = YES;
-    Card *card = [self.deck drawRandomCard];
-    //NSLog(card.contents);
-    
-    if (card) {
-        [sender setTitle:card.contents forState:UIControlStateSelected];
-        self.flipCount++;
-    
-    } else {
-        NSLog(@"End of deck reached!");
-    }
-  */
-    
+    [self.game flipCardAtIndex: [self.cardButtons indexOfObject:sender]];
+    self.flipCount++;
+    [self updateUI];
+    /*
+     sender.selected = YES;
+     Card *card = [self.deck drawRandomCard];
+     //NSLog(card.contents);
+
+     if (card) {
+     [sender setTitle:card.contents forState:UIControlStateSelected];
+     self.flipCount++;
+
+     } else {
+     NSLog(@"End of deck reached!");
+     }
+     */
+
 }
 
 -(void)setFlipCount:(int)flipCount  {
@@ -61,6 +72,7 @@
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
     NSLog(@"Flips updated to %d",self.flipCount);
 }
+
 
 
 @end
